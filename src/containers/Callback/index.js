d78bf3364c4c6ac0f217809895ac258adb466ef1
Jose Navarro <jmn8718@gmail.com>
2018-09-10 16:24:41 +0200

    refactor callback container

--- src/containers/Callback/index.js
+++ src/containers/Callback/index.js
@@ -1,39 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import loading from "./loading.svg";
 
 import { Auth } from "../../controllers";
 
-class Callback extends Component {
-  handleAuthentication = ({ location }) => {
-    if (/access_token|id_token|error/.test(location.hash)) {
-      Auth.handleAuthentication();
-    }
-  };
-
-  componentWillMount() {
-    this.handleAuthentication(this.props);
+const handleAuthentication = ({ location }) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    Auth.handleAuthentication();
   }
+};
 
-  render() {
-    const style = {
-      position: "absolute",
-      display: "flex",
-      justifyContent: "center",
-      height: "100vh",
-      width: "100vw",
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: "white"
-    };
+const STYLE = {
+  position: "absolute",
+  display: "flex",
+  justifyContent: "center",
+  height: "100vh",
+  width: "100vw",
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+  backgroundColor: "white"
+};
 
-    return (
-      <div style={style}>
-        <img src={loading} alt="loading" />
-      </div>
-    );
-  }
-}
+const Callback = props => {
+  handleAuthentication(props);
+
+  return (
+    <div style={STYLE}>
+      <img src={loading} alt="loading" />
+    </div>
+  );
+};
 
 export default Callback;
