5a3e1fe165ded0512ea36f6cef06f21d31b99844
Jose Navarro <jmn8718@gmail.com>
2019-02-05 15:46:17 +0100

    update home container

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Button from '@material-ui/core/Button';
+import { withStyles } from "@material-ui/core/styles";
+import { IDP_LOGIN } from '../../config';
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  }
+});
 
 class Home extends Component {
   state = {};
   render() {
-    return <div>Home</div>;
+    const { classes } = this.props;
+    return (
+      <div>
+        <h2>Home</h2>
+        
+        {IDP_LOGIN && <Button
+          variant="contained"
+          color="primary"
+          href={IDP_LOGIN}
+          className={classes.button}
+        >
+          Initiate IdP
+        </Button>}
+      </div>
+    );
   }
 }
-export default Home;
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Home);
